430e8610308c1f66050238884794e1b8a7ced58f
fabius8 <fabius8@163.com>
2018-05-22 12:53:12 +0800

    haha

--- start.sh
+++ start.sh
@@ -3,9 +3,4 @@
 docker build -t fabius8/ulordrig .
 
 echo "start to run ulordrig"
-docker run -d --name ulordrig \
-       -e POOL_ADDRESS=117.45.165.141:7200 \
-       -e PAYMENT_ADDRESS=ug2vVSuZWqPg47xHBz1zkpwDCtCUAEn8v5 \
-       -e NAME=noname \
-       -e THREADS=1 \
-      fabius8/ulordrig
+docker run -d --name ulordrig fabius8/ulordrig
