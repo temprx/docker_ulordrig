a047ac6db4d5e36983202c142634229b0bcf7558
temprx <41691822+temprx@users.noreply.github.com>
2018-07-30 13:40:59 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-
+cat > config.txt <<CONFIG
+"call_timeout": 10,
+"retry_time": 30,
+"giveup_limit": 0,
+"verbose_level": $VERBOSE,
+"print_motd": true,
+"h_print_time": 60,
+"aes_override": null,
+"use_slow_memory": "warn",
+"tls_secure_algo": true,
+"daemon_mode": false,
+"flush_stdout": true,
+"output_file": "",
+"httpd_port": $PORT,
+"http_login": "",
+"http_pass": "",
+"prefer_ipv4": true,
+CONFIG
 docker build -t fabius8/ulordrig .
 
 echo "start to run ulordrig"
